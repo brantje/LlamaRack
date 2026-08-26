8a70cb5788fddc15581b9b8813b32c5b4361662d
Sander <brantje@gmail.com>
2026-08-26 12:09:48 +0200

    Make frontend API host LAN-aware

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,7 +2,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8888'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || ''
     }
   },
   app: {
