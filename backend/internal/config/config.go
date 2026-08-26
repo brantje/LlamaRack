6717b88cd9d3f4997dd0308f775b86336773bcf1
Sander <brantje@gmail.com>
2026-08-26 15:21:42 +0200

    feat: configure always-on reconcile interval

--- backend/internal/config/config.go
+++ backend/internal/config/config.go
@@ -8,33 +8,39 @@ import (
 )
 
 type Config struct {
-	ListenAddr      string
-	DataDir         string
-	ModelsDir       string
-	DatabasePath    string
-	LlamaServerPath string
-	WorkerHost      string
-	WorkerPortStart int
-	StartupTimeout  time.Duration
-	SessionLifetime time.Duration
-	AllowedOrigin   string
+	ListenAddr                string
+	DataDir                   string
+	ModelsDir                 string
+	DatabasePath              string
+	LlamaServerPath           string
+	WorkerHost                string
+	WorkerPortStart           int
+	StartupTimeout            time.Duration
+	SessionLifetime           time.Duration
+	AllowedOrigin             string
+	AlwaysOnReconcileInterval time.Duration
 }
 
 func Load() Config {
 	dataDir := env("LCM_DATA_DIR", "/config")
 	workerPort, _ := strconv.Atoi(env("LCM_WORKER_PORT_START", "10000"))
 	startupSeconds, _ := strconv.Atoi(env("LCM_STARTUP_TIMEOUT_SECONDS", "180"))
+	alwaysOnSeconds, err := strconv.Atoi(env("LCM_ALWAYS_ON_RECONCILE_SECONDS", "15"))
+	if err != nil || alwaysOnSeconds < 0 {
+		alwaysOnSeconds = 15
+	}
 	return Config{
-		ListenAddr:      env("LCM_LISTEN_ADDR", ":8000"),
-		DataDir:         dataDir,
-		ModelsDir:       env("LCM_MODELS_DIR", "/models"),
-		DatabasePath:    env("LCM_DATABASE_PATH", filepath.Join(dataDir, "manager.db")),
-		LlamaServerPath: env("LCM_LLAMA_SERVER", "llama-server"),
-		WorkerHost:      env("LCM_WORKER_HOST", "127.0.0.1"),
-		WorkerPortStart: workerPort,
-		StartupTimeout:  time.Duration(startupSeconds) * time.Second,
-		SessionLifetime: 24 * time.Hour,
-		AllowedOrigin:   env("LCM_ALLOWED_ORIGIN", "http://localhost:3000"),
+		ListenAddr:                env("LCM_LISTEN_ADDR", ":8000"),
+		DataDir:                   dataDir,
+		ModelsDir:                 env("LCM_MODELS_DIR", "/models"),
+		DatabasePath:              env("LCM_DATABASE_PATH", filepath.Join(dataDir, "manager.db")),
+		LlamaServerPath:           env("LCM_LLAMA_SERVER", "llama-server"),
+		WorkerHost:                env("LCM_WORKER_HOST", "127.0.0.1"),
+		WorkerPortStart:           workerPort,
+		StartupTimeout:            time.Duration(startupSeconds) * time.Second,
+		SessionLifetime:           24 * time.Hour,
+		AllowedOrigin:             env("LCM_ALLOWED_ORIGIN", "http://localhost:3000"),
+		AlwaysOnReconcileInterval: time.Duration(alwaysOnSeconds) * time.Second,
 	}
 }
 
