c6b0568974e6100b1927989cd338d6dc9a7aca9e
Sander <brantje@gmail.com>
2026-08-27 11:58:07 +0200

    Remove legacy Discover route

--- frontend/app/pages/discover.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import ModelsDiscover from '~/components/ModelsDiscover.vue'
-
-definePageMeta({ redirect: '/models/discover' })
-</script>
-
-<template>
-  <ModelsDiscover />
-</template>
