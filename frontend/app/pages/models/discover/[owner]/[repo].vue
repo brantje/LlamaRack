191e36591ecb7fc51447a89432e850c60d1045c9
Sander <brantje@gmail.com>
2026-08-28 12:55:21 +0200

    feat: add routable model discovery detail page

---
+++ frontend/app/pages/models/discover/[owner]/[repo].vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ModelsDiscover from '~/components/ModelsDiscover.vue'
+
+const route = useRoute()
+const repoID = computed(() => `${String(route.params.owner || '')}/${String(route.params.repo || '')}`)
+</script>
+
+<template>
+  <ModelsDiscover :repo-id="repoID" />
+</template>
