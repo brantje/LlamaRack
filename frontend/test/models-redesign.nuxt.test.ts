16ae05a332153b02304eb16f3bf250d04558fb73
Sander <brantje@gmail.com>
2026-08-30 00:06:13 +0200

    #39 Fix Models redesign test imports

--- frontend/test/models-redesign.nuxt.test.ts
+++ frontend/test/models-redesign.nuxt.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { mountSuspended } from '@nuxt/test-utils/runtime'
+import { mockNuxtImport, mountSuspended } from '@nuxt/test-utils/runtime'
 import ModelsPage from '~/pages/models/index.vue'
 import AppButton from '~/components/AppButton.vue'
 import { useManager } from '~/composables/useManager'
